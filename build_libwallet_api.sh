65338d25ba18ed3dfaf0a1c5ccc3d79287a64bbc
OT-BareHands <41138528+OT-BareHands@users.noreply.github.com>
2018-07-12 15:10:54 +0800

    ETN-Core Wallet-GUI Part 1

---
+++ build_libwallet_api.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ELECTRONEUM_URL=https://github.com/electroneumclassic/electroneum-classic.git
+# ELECTRONEUM_BRANCH=master
+CPU_CORE_COUNT=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+pushd $(pwd)
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $ROOT_DIR/utils.sh
+
+
+INSTALL_DIR=$ROOT_DIR/wallet
+ELECTRONEUM_DIR=$ROOT_DIR/electroneum
+
+
+mkdir -p $ELECTRONEUM_DIR/build/release
+pushd $ELECTRONEUM_DIR/build/release
+
+# reusing function from "utils.sh"
+platform=$(get_platform)
+
+pushd $ELECTRONEUM_DIR/build/release/src/wallet
+make -j$CPU_CORE_COUNT
+make install -j$CPU_CORE_COUNT
+popd
+
+# unbound is one more dependency. can't be merged to the wallet_merged
+# since filename conflict (random.c.obj)
+# for Linux, we use libunbound shipped with the system, so we don't need to build it
+
+if [ "$platform" != "linux" ]; then
+    echo "Building libunbound..."
+    pushd $ELECTRONEUM_DIR/build/release/external/unbound
+    # no need to make, it was already built as dependency for libwallet
+    # make -j$CPU_CORE_COUNT
+    make install -j$CPU_CORE_COUNT
+    popd
+fi
+
+popd
